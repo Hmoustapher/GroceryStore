f29a603a752c088c7eb33fae758349937ec2f4f0
hmoustapher <hmoustapher@gmail.com>
2025-01-15 12:27:47 +0000

    Adding getproduct

--- Backend/products_dao.py
+++ Backend/products_dao.py
@@ -1,1 +1,13 @@
 from sql_connection import get_sql_connection
+
+def get_all_products(connection):
+    try:
+        cursor = connection.cursor()
+        query = """
+            SELECT products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name
+            FROM products
+            INNER JOIN uom
+            ON products.uom_id = uom.uom_id
+        """
+        cursor.execute(query)
+
