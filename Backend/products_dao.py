1612346c0d0b919a77b20fc2907c3829fd98dc65
hmoustapher <hmoustapher@gmail.com>
2025-01-15 12:32:48 +0000

    delete product

--- Backend/products_dao.py
+++ Backend/products_dao.py
@@ -48,3 +48,20 @@ def insert_new_product(connection, product):
     connection.commit()
 
     return cursor.lastrowid
+
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+    query = ("DELETE FROM products where product_id=" + str(product_id))
+    cursor.execute(query)
+    connection.commit()
+
+    return cursor.lastrowid
+
+if __name__ == '__main__':
+    connection = get_sql_connection()
+    # print(get_all_products(connection))
+    print(insert_new_product(connection, {
+        'product_name': 'potatoes',
+        'uom_id': '1',
+        'price_per_unit': 10
+    }))
