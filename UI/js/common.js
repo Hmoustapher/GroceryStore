d49fde14666630e97c3d574b515b33d80bd4cf02
hmoustapher <hmoustapher@gmail.com>
2025-01-20 13:58:53 +0000

    adding dropdown function

--- UI/js/common.js
+++ UI/js/common.js
@@ -5,3 +5,22 @@ var productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';
 var productDeleteApiUrl = 'http://127.0.0.1:5000/deleteProduct';
 var orderListApiUrl = 'http://127.0.0.1:5000/getAllOrders';
 var orderSaveApiUrl = 'http://127.0.0.1:5000/insertOrder';
+
+// For product dropdown in order
+var productsApiUrl = 'https://fakestoreapi.com/products';
+
+function callApi(method, url, data) {
+    $.ajax({
+        method: method,
+        url: url,
+        data: data,
+        success: function(msg) {
+            // Handle success case, e.g., update UI
+            console.log("API call successful", msg);
+            // window.location.reload(); // Uncomment if you want to reload the page upon success
+        },
+        error: function(xhr, status, error) {
+            console.error("API call failed with status: " + status + ", Error: " + error);
+        }
+    });
+}
